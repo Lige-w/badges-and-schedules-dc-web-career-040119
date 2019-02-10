1dc72b9c99727891bb5655dd85b498e40770b96a
Lige <lige.williamson@gmail.com>
2019-02-10 14:24:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,8 @@ def assign_rooms(speakers)
   room_assignments = []
   speakers.each_with_index do |name, index|
     room_assignments << "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+  room_assignments
 end
 
   
