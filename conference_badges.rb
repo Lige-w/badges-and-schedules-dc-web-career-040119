35f7ea212c21096b555f9c5d9789ff4d8b9aea2f
Lige <lige.williamson@gmail.com>
2019-02-10 14:10:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def  badge_maker(array)
-  return array.each {|name| "Hello, my name is #{name}"}
+  array.each {|name| return "Hello, my name is #{name}"}
 end
 
 puts badge_maker([1, 2, 3])
