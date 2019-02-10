d33a3ae0e35c0e64a49bb45fc0dec525b91275b2
Lige <lige.williamson@gmail.com>
2019-02-10 14:24:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,16 @@ def  badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(speakers)
   badges = []
-  array.each {|name| badges << badge_maker(name)}
+  speakers.each {|name| badges << badge_maker(name)}
   badges
 end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index}!"
+end
+  
+  
